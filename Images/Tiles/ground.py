ad165d269c192864c56129072e4eb20b99489c7b
Joe <72451810+JoeMuzzin@users.noreply.github.com>
2021-05-26 15:55:37 -0400

    groundtiles

--- Images/Tiles/ground.py
+++ Images/Tiles/ground.py
@@ -6,10 +6,8 @@ class ground(pygame.sprite.Sprite):
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        blackcube = Surface((100, 100))
-        blackcube.fill((0,0,0))
-        self.image = blackcube
-        self.rect = blackcube.get_rect()
+        self.image = pygame.image.load("./groundTILE.png")
+        self.rect = self.image.get_rect()
         self.width = self.image.get_width()
         self.height = self.image.get_height()
         self.rect.center = (self.width/2, self.height/2)
