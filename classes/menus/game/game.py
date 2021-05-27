27d352375643831cc486ad02302ff4b1f1775b08
ZachKelley <kelley1zr@alma.edu>
2021-05-27 16:38:23 -0400

    update

--- classes/menus/game/game.py
+++ classes/menus/game/game.py
@@ -6,14 +6,30 @@ from classes.objects.enemies.laserflea import laserflea
 from classes.objects.enemies.poisonchomper import poisonchomper
 from classes.objects.player.player import Player
 
-test = laserflea("right")
-test2 = poisonchomper("right")
+Boss = laserflea("right")
+chompa1 = poisonchomper("right")
+chompa2 = poisonchomper("left")
+chompa2.rect.x = chompa2.rect.x + 1200
+chompa3 = poisonchomper("left")
+chompa3.rect.x = chompa2.rect.x + 100
+chompa4 = poisonchomper("left")
+chompa4.rect.x = chompa2.rect.x + 200
+chompa5 = poisonchomper("right")
+chompa5.rect.x = chompa1.rect.x - 100
+chompa6 = poisonchomper("right")
+chompa6.rect.x = chompa1.rect.x - 200
+
 player = Player("right")
-player.rect.x = Config.map.get_width()
+player.rect.x = 0
 player.rect.y = Config.map.get_height()/6
 test_sprites = pygame.sprite.Group()
-test_sprites.add(test)
-test_sprites.add(test2)
+test_sprites.add(Boss)
+test_sprites.add(chompa1)
+test_sprites.add(chompa2)
+test_sprites.add(chompa3)
+test_sprites.add(chompa4)
+test_sprites.add(chompa5)
+test_sprites.add(chompa6)
 player_sprite.add(player)
 Config.map.convert()
 
@@ -22,9 +38,13 @@ def run():
     Config.map = pygame.transform.flip(map, False, False)
     for t in test_sprites:
         pygame.display.update(t.rect)
+        if t.health <= 0:
+            test_sprites.remove(t)
     for p in player_sprite:
         p.bullets.draw(Config.map)
         p.bullets.update()
+        if p.health <= 0:
+            player_sprite.remove(p)
         for b in p.bullets:
             pygame.display.update(b.rect)
         pygame.display.update(p.rect)
--- classes/objects/enemies/poisonchomper.py
+++ classes/objects/enemies/poisonchomper.py
@@ -24,19 +24,23 @@ class poisonchomper(pygame.sprite.Sprite):
 
     idle = pygame.image.load("./Images/enemies/poison-chomper/idle.png")
     idleleft = Animation(idle, 175, 119)
+    idleleft.scalefactor = 32
     idleleft.build()
     idleleft = idleleft.animation
     idleright = Animation(idle, 175, 119, flip=True)
+    idleright.scalefactor = 32
     idleright.build()
     idleright = idleright.animation
 
+
     jump = pygame.image.load("./Images/enemies/poison-chomper/jump-cycle.png")
-    jumpleft = Animation(jump, 304, 246)
+    jumpleft = Animation(jump, 304, 246, num=1)
     jumpleft.build()
     jumpleft = jumpleft.animation
-    jumpright = Animation(jump, 304, 246, flip=True)
+    jumpright = Animation(jump, 304, 246, flip=True, num=1)
     jumpright.build()
     jumpright = jumpright.animation
+
 
     animationdic = {
         0: idleleft,
@@ -45,8 +49,12 @@ class poisonchomper(pygame.sprite.Sprite):
         3: jumpright,
     }
 
+    jumploop = 0
+    hitcounter = 0
+
     def __init__(self, direction):
         pygame.sprite.Sprite.__init__(self)
+        self.grounded = True
         if direction == "right":
             self.state = self.states.IDLERIGHT
             self.animation = self.animationdic[self.state]
@@ -58,18 +66,17 @@ class poisonchomper(pygame.sprite.Sprite):
             self.image = self.animation[0]
             self.facing = "left"
         self.index = 0
-        self.width = self.image.get_width()
-        self.height = self.image.get_height()
         self.rect = self.image.get_rect()
-        self.rect.center = (self.width / 2, self.height / 2)
+        self.rect.center = (self.rect.width / 2, self.rect.height / 2)
         self.rect.x = Config.WIDTH/2
-        self.rect.y = Config.HEIGHT/2
-        self.rect.x = 2000
-        self.rect.y = Config.HEIGHT / 2
+        self.rect.y = Config.HEIGHT/5
+        self.hitmask = pygame.mask.from_surface(self.image)
+        self.health = 50
 
     def update(self):
+        self.hitmask = pygame.mask.from_surface(self.image)
 
-        if self.counter >= 5:
+        if self.counter >= 1:
             self.index += 1
             self.counter = 0
 
@@ -80,6 +87,7 @@ class poisonchomper(pygame.sprite.Sprite):
                 self.index = 0
                 self.state = self.states.JUMPLEFT
                 self.animation = self.animationdic[self.state]
+                self.rect.y -= 32
         elif self.state == self.states.IDLERIGHT:
             self.image = self.animation[self.index]
             self.facing = "right"
@@ -87,25 +95,81 @@ class poisonchomper(pygame.sprite.Sprite):
                 self.index = 0
                 self.state = self.states.JUMPRIGHT
                 self.animation = self.animationdic[self.state]
+                self.rect.y -= 32
         elif self.state == self.states.JUMPLEFT:
             self.image = self.animation[self.index]
-            self.rect.x -= 3
             self.facing = "left"
-            if self.index == len(self.animation) - 1:
+            if self.jumploop < 10:
+                self.rect.x -= 10
+                self.rect.y -= 5
+                if self.index == len(self.animation) - 1:
+                    self.index = 0
+                    self.jumploop += 1
+            else:
                 self.index = 0
                 self.state = self.states.IDLERIGHT
                 self.animation = self.animationdic[self.state]
+                self.rect.y += 32
+                self.jumploop = 0
+
         elif self.state == self.states.JUMPRIGHT:
             self.image = self.animation[self.index]
-            self.rect.x += 3
-            self.facing = "left"
-            if self.index == len(self.animation) - 1:
-                self.index = 0
-                self.state = self.states.IDLELEFT
-                self.animation = self.animationdic[self.state]
+            self.facing = "right"
+            if self.jumploop < 10:
+                self.rect.x += 10
+                self.rect.y -= 5
+                if self.index == len(self.animation) - 1:
+                    self.index = 0
+                    self.jumploop += 1
+            else:
+                if self.index == len(self.animation) - 1:
+                    self.index = 0
+                    self.state = self.states.IDLELEFT
+                    self.animation = self.animationdic[self.state]
+                    self.jumploop = 0
 
-        if self.rect.bottom >= Config.HEIGHT:
-            self.rect.bottom = Config.HEIGHT
+        self.grounded = False
+        for g in ground_sprites:
+            if self.rect.bottom <= g.rect.top + 10:
+                if g.rect.left < self.rect.x < g.rect.right or (g.rect.left < self.rect.right < g.rect.right):
+                    if self.rect.bottom >= g.rect.top:
+                        if self.state != self.states.JUMPLEFT or self.state != self.states.JUMPRIGHT:
+                            self.rect.bottom = g.rect.top
+                            self.grounded = True
+        if not self.grounded:
+            self.rect.y += 5
 
-        pygame.draw.rect(Config.WIN, Config.RED, self.rect, 2)
+        for p in player_sprite:
+            px = p.rect.x
+            py = p.rect.y
+            sx = self.rect.x
+            sy = self.rect.y
+
+            offset = (px - sx, py - sy)
+            result = self.hitmask.overlap(p.hitmask, offset)
+            if result:
+                if self.hitcounter > 10:
+                    if p.health > 0:
+                        p.damage(1)
+                    self.hitcounter = 0
+            for b in p.bullets:
+                bx = b.rect.x
+                by = b.rect.y
+                sx = self.rect.x
+                sy = self.rect.y
+
+                offset = (bx - sx, by - sy)
+                result = self.hitmask.overlap(b.hitmask, offset)
+                if result:
+                    p.bullets.remove(b)
+                    if self.health > 0:
+                        self.health -= 5
+
+        pygame.draw.line(Config.map, Config.RED, (self.rect.x, self.rect.y - 10), (self.rect.right, self.rect.y - 10),
+                         4)
+        pygame.draw.line(Config.map, Config.GREEN, (self.rect.x, self.rect.y - 10),
+                         (self.rect.x + ((self.health / 50) * self.rect.width), self.rect.y - 10), 4)
+
+
         self.counter += 1
+        self.hitcounter += 1
